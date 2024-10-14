c5f593c19a8e04a65c2c63845de07ed636980a15
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-10-14 11:34:52 +0530

    add the css file on it

--- src/Components/Pages/ContactUs.jsx
+++ src/Components/Pages/ContactUs.jsx
@@ -1,6 +1,6 @@
 import React  from "react";
 import {Container , Row  , Col , Form , Button} from "react-bootstrap";
-
+import '../cssFile/contact.css'
 const ContactUs = () =>{
    return(<>
         <Container className="contact-us-section my-5 ">
